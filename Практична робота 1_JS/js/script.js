28ce72e58edaddc50090750a5e32ba2b1087dd59
Napster-YT <a.abdusaliamov@gmail.com>
2021-02-13 22:19:52 +0200

    pw-1

---
+++ Практична робота 1_JS/js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let str = 'Привіт';
+let num = 123;
+let flag = true;
+let txt = 'true'
+
+console.log(typeof str);//string
+console.log(typeof num);//number
+console.log(typeof flag);//boolean
+console.log(typeof txt);//string
+
+let age = 8;
+const name = 'Peter';
+const surname = 'Agaser';
+const mum = 'Anna';
+const dad = 'Genre';
+const brothers = 'Manny and Teger.';
+// const isBoy = true;
+
+console.log('Ім`я нашого героя: ' + name + ', прізвище: ' + surname + ', вік: ' + age + '. Його мати: ' + mum +', його батько: '+ dad + ', його брати: ' + brothers);
