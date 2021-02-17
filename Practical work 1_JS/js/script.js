1cf1f3df835d9011894a30dedb21c4144d8dffcc
Napster-YT <a.abdusaliamov@gmail.com>
2021-02-17 16:36:45 +0200

    Practical work 1

--- Practical work 1_JS/js/script.js
+++ Practical work 1_JS/js/script.js
@@ -1,21 +1,26 @@
 'use strict'
-
-let str = 'Привіт';
-let num = 123;
-let flag = true;
-let txt = 'true'
+// ----------------------------------------№1----------------------------------------
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = 'true'
 
 console.log(typeof str);//string
 console.log(typeof num);//number
 console.log(typeof flag);//boolean
 console.log(typeof txt);//string
 
-let age = 8;
+// ----------------------------------------№2----------------------------------------
 const name = 'Peter';
 const surname = 'Agaser';
+let age = 8;
 const mum = 'Anna';
 const dad = 'Genre';
 const brothers = 'Manny and Teger.';
+let hobby = 'Painting, Literature, Maths.'
+let theCountryWhereHeLives = 'France';
+let theCityWhereHeLives = 'Paris';
+let schoolWhereHeStuding = 'Secondary school №35';
 // const isBoy = true;
 
-console.log('Ім`я нашого героя: ' + name + ', прізвище: ' + surname + ', вік: ' + age + '. Його мати: ' + mum +', його батько: '+ dad + ', його брати: ' + brothers);
+console.log('Ім`я нашого героя: ' + name + ', прізвище: ' + surname + ', вік: ' + age + '. Його мати: ' + mum +', його батько: '+ dad + ', його брати: ' + brothers + ' Його хобі: ' +hobby+' Країна, де він живе: '+theCountryWhereHeLives+'. Місто, де він живе: '+theCityWhereHeLives+'. Школа, в якій він вчиться: '+schoolWhereHeStuding+'.');
